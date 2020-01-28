fb2e5ef0d9ca6a39afd449c12979e556a71418bf
aakashDeveloper <ahanda205@gmail.com>
2020-01-28 16:27:06 +0000

    a

---
+++ myapp/src/component/Home.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import Header from './Header';
+import NewsList from './NewsList';
+import JSON from '../db.json';
+
+class Home extends Component {
+    
+    constructor(){
+        super()
+
+        this.state={
+            news:JSON
+        }
+    }
+    render(){
+        console.log(JSON);
+        return(
+            <React.Fragment>
+                <Header/>
+                <NewsList/>
+            </React.Fragment>
+        )  
+    }
+}
+
+export default Home;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './component/Header'
+import Home from './component/Home';
 
-const App = () => {
-    return(
-        <React.Fragment>
-            <Header/>
-            <h1>Welcome to react</h1>
-            <h2>This is from Nareshit</h2>
-        </React.Fragment>
-    )
-}
-
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<Home/>,document.getElementById('root'));
