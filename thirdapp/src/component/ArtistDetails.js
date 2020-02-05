4159378dd795d53b89530bfe0d39a952007d8dd1
aakashDeveloper <ahanda205@gmail.com>
2020-02-05 16:15:57 +0000

    a

--- thirdapp/src/component/ArtistDetails.js
+++ thirdapp/src/component/ArtistDetails.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import Header from './Header';
+import Albums from './Albums';
 
+const url = "http://localhost:8900/artists"
 class ArtistDetails extends Component {
     constructor(props){
         super(props)
@@ -9,15 +13,46 @@ class ArtistDetails extends Component {
         }
     }
     render(){
+        console.log(">>>",this.state.details)
+        const mydata = this.state.details;
         return(
-            <div>
-                Details
-            </div>
+            <React.Fragment>
+                <Header/>
+                <div className="artist_bio">
+                    <div className="artist_image">
+                        <span style={{background:`url('/images/covers/${this.state.details.cover}.jpg')`}}>
+                        </span>
+                        <h3>{mydata.name}</h3>
+                        <div className="bio_text">
+                            {this.state.details.bio}
+                        </div>
+
+                    </div>
+                </div>
+                <Albums albumdata={mydata.albums}/>
+            </React.Fragment>
         )
     }
 
-    componentDidMount(){
+    /*componentDidMount(){
         console.log(this.props.match.params.id)
+        axios.get(`${url}/${this.props.match.params.id}`)
+        .then(response => {
+            this.setState({
+                details:response.data
+            })
+        })
+        .catch((err) => {
+            throw err
+        })
+    }*/
+
+    async componentDidMount(){
+        const response = await axios.get(`${url}/${this.props.match.params.id}`)
+        const mydata = response.data
+        this.setState({
+            details:mydata
+        })
     }
 }
 
