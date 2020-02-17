1a3a3dca85470685ea69c9261bcb0c35bf8756f5
aakashDeveloper <ahanda205@gmail.com>
2020-02-17 16:36:34 +0000

    a

--- redux2/src/actions/index.js
+++ redux2/src/actions/index.js
@@ -1,11 +1,22 @@
 const url = "http://localhost:8900"
 
+
 export function latestNews(){
     const output = fetch(`${url}/articles?_end=3`, {method:'GET'})
     .then((data) => data.json())
 
     return{
         type:'GET_LATEST',
+        payload:output
+    }
+}
+
+export function articleNews(){
+    const output = fetch(`${url}/articles?_start=3&_end=10`, {method:'GET'})
+    .then((data) => data.json())
+
+    return{
+        type:'GET_ARTICLES',
         payload:output
     }
 }
--- redux2/src/container/Home.js
+++ redux2/src/container/Home.js
@@ -1,18 +1,23 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
-import { latestNews} from '../actions';
+import { latestNews,articleNews} from '../actions';
 import { bindActionCreators } from 'redux';
+
+import LatestDisplay from '../component/Home/latestdisplay';
+import ArticleDisplay from '../component/Home/ArticleDisplay';
 
 
 class Home extends Component{
     componentDidMount(){
         this.props.latestNews();
+        this.props.articleNews()
     }
 
     render(){
         return(
             <div>
-                <h1>Home</h1>
+                <LatestDisplay latdata={this.props.datalist.latestdata}/>
+                <ArticleDisplay artdata={this.props.datalist.articledata}/>
             </div>
         )
     }
@@ -20,10 +25,13 @@ class Home extends Component{
 
 function mapStateToProps(state){
     console.log(state)
+    return{
+        datalist:state.articles
+    }
 }
 
 function mapDispatchToProps(dispatch){
-    return bindActionCreators({latestNews}, dispatch)
+    return bindActionCreators({latestNews,articleNews}, dispatch)
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Home);
